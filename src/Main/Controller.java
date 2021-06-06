a51849301946f8e7d3a696a0505335c7a8704451
YaelPM <193259@ids.upchiapas.edu.mx>
2021-06-05 19:47:47 -0500

    Juego funcional

--- src/Main/Controller.java
+++ src/Main/Controller.java
@@ -3,65 +3,224 @@ package Main;
 import Main.Model.ThreadClient;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.ObservableFaceArray;
-
 
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.ResourceBundle;
 
-public class Controller implements  Observer {
+public class Controller implements  Observer, Initializable {
     private Socket socket;
     private DataOutputStream bufferDeSalida = null;
+    private ArrayList<Label> labels= new ArrayList<>();
+    private boolean turno= true;
+    private boolean ok=true;
+    private int wins=0;
+    private int loses=0;
+    int matriz[][]={
+            {0,1,2},
+            {3,4,5},
+            {6,7,8},
+            {0,3,6},
+            {1,4,7},
+            {2,5,8},
+            {0,4,8},
+            {2,4,6}
+    };
 
+    @FXML
+    private Label labelConectado;
+    @FXML
+    private Label labelEsperar;
+    @FXML
+    private Label labelWins;
+    @FXML
+    private Label labelLoses;
     @FXML
     private Button btnSalir;
-
     @FXML
     private Button btnConectar;
-
     @FXML
-    private TextField ipServer;
-
+    private Button btnReiniciar;
     @FXML
-    private TextField portServer;
-
+    private GridPane grid;
     @FXML
-    private TextArea log;
-
+    private Label labelOfName;
     @FXML
-    private TextField txtEnviar;
-
+    private Label label0;
     @FXML
-    private Button btnEnviar;
-
+    private Label label1;
     @FXML
-    private Circle circle;
+    private Label label3;
+    @FXML
+    private Label label2;
+    @FXML
+    private Label label4;
+    @FXML
+    private Label label5;
+    @FXML
+    private Label label6;
+    @FXML
+    private Label label7;
+    @FXML
+    private Label label8;
 
     @FXML
     void btnConectarOnMouseClicked(MouseEvent event) {
+        conectar();
+    }
+
+    @FXML
+    void reiniciarOnMouseClicked(MouseEvent event) {
+        reiniciar();
         try {
-            socket = new Socket(ipServer.getText(), Integer.valueOf(portServer.getText()));
-            log.setText( "Creado");
+            bufferDeSalida.writeUTF("Nodo0-"+"O:"+"10");
+            bufferDeSalida.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void reiniciar(){
+        for (int x=0; x<labels.size(); x++){
+            labels.get(x).setText("");
+            labels.get(x).setStyle("-fx-background-color: black; -fx-border-color: white; -fx-border-radius: 5;");
+        }
+        labelEsperar.setText("");
+        turno=true;
+        ok=true;
+    }
+
+    public void comprobarGanador(){
+        for (int x=0; x<matriz.length; x++){
+            if (labels.get(matriz[x][0]).getText().equals("X") &&
+                    labels.get(matriz[x][1]).getText().equals("X") &&
+                        labels.get(matriz[x][2]).getText().equals("X")){
+                loses++;
+                String w= String.valueOf(loses);
+                labelLoses.setText(w);
+                ok=false;
+                labels.get(matriz[x][0]).setStyle("-fx-background-color: red; -fx-border-color: white; -fx-border-radius: 5;");
+                labels.get(matriz[x][1]).setStyle("-fx-background-color: red; -fx-border-color: white; -fx-border-radius: 5;");
+                labels.get(matriz[x][2]).setStyle("-fx-background-color: red; -fx-border-color: white; -fx-border-radius: 5;");
+            }
+            if (labels.get(matriz[x][0]).getText().equals("O") &&
+                    labels.get(matriz[x][1]).getText().equals("O") &&
+                        labels.get(matriz[x][2]).getText().equals("O")){
+                wins++;
+                String w= String.valueOf(wins);
+                labelWins.setText(w);
+                ok=false;
+                labels.get(matriz[x][0]).setStyle("-fx-background-color: green; -fx-border-color: white; -fx-border-radius: 5;");
+                labels.get(matriz[x][1]).setStyle("-fx-background-color: green; -fx-border-color: white; -fx-border-radius: 5;");
+                labels.get(matriz[x][2]).setStyle("-fx-background-color: green; -fx-border-color: white; -fx-border-radius: 5;");
+            }
+        }
+    }
+
+    public static boolean casillaLibre(Label l){
+        if(l.getText().equals("")){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public void presionarCasilla(Label label, int posicion){
+        String idx= String.valueOf(posicion);
+        if(ok){
+            if(casillaLibre(label) && turno){
+                label.setText("O");
+                try {
+                    bufferDeSalida.writeUTF("Nodo0-"+"O:"+idx);
+                    bufferDeSalida.flush();
+                    turno= false;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                comprobarGanador();
+            }else{
+                labelEsperar.setText("Espera tu turno");
+            }
+        }else{
+            labelEsperar.setText("Dale Reiniciar");
+        }
+
+    }
+
+    @FXML
+    void label0OnMouseClicked(MouseEvent event) {
+        presionarCasilla( label0, 0);
+    }
+
+    @FXML
+    void label1OnMouseClicked(MouseEvent event) {
+        presionarCasilla( label1, 1);
+    }
+
+    @FXML
+    void label2OnMouseClicked(MouseEvent event) {
+        presionarCasilla( label2, 2);
+    }
+
+    @FXML
+    void label3OnMouseClicked(MouseEvent event) {
+        presionarCasilla( label3, 3);
+    }
+
+    @FXML
+    void label4OnMouseClicked(MouseEvent event) {
+        presionarCasilla( label4, 4);
+    }
+
+    @FXML
+    void label5OnMouseClicked(MouseEvent event) {
+        presionarCasilla( label5, 5);
+    }
+
+    @FXML
+    void label6OnMouseClicked(MouseEvent event) {
+        presionarCasilla( label6, 6);
+    }
+
+    @FXML
+    void label7OnMouseClicked(MouseEvent event) {
+        presionarCasilla( label7, 7);
+    }
+
+    @FXML
+    void label8OnMouseClicked(MouseEvent event) {
+        presionarCasilla( label8, 8);
+    }
+
+    void conectar(){
+        try {
+            socket = new Socket("192.168.1.68", 3001);
+            labelConectado.setText( "Conectado...");
             bufferDeSalida = new DataOutputStream(socket.getOutputStream());
             bufferDeSalida.flush();
-
-            ThreadClient cliente = new ThreadClient(socket,log);
+            ThreadClient cliente = new ThreadClient(socket);
             cliente.addObserver(this);
             new Thread(cliente).start();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
     @FXML
     void btnSalirOnMouseClicked(MouseEvent event) {
         Platform.exit();
@@ -79,34 +238,32 @@ public class Controller implements  Observer {
         }
     }
 
-    @FXML
-    void btnEnviarOnMouseClicked(MouseEvent event) {
-        try {
-            bufferDeSalida.writeUTF(txtEnviar.getText());
-            bufferDeSalida.writeUTF("1:1");
-            bufferDeSalida.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
+    @Override
+    public void update(Observable o, Object arg) {
+        String pos = (String) arg;
+        int position = Integer.parseInt(pos);
+        if (position!=10){
+            Platform.runLater(() -> labels.get(position).setText("X"));
+            Platform.runLater(()-> comprobarGanador());
+            Platform.runLater(()-> labelEsperar.setText(""));
+        }else{
+            Platform.runLater(()-> reiniciar());
+            Platform.runLater(()-> labelEsperar.setText(""));
         }
 
+        turno=true;
     }
 
     @Override
-    public void update(Observable o, Object arg) {
-        String color = (String) arg;
-        switch (color){
-            case "1":
-                circle.setFill(Color.RED);
-                break;
-            case "2":
-                circle.setFill(Color.GREEN);
-                break;
-            case "3":
-                circle.setFill(Color.BLUE);
-                break;
-            case "4":
-                circle.setFill(Color.YELLOW);
-                break;
-        }
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        labels.add(label0);
+        labels.add(label1);
+        labels.add(label2);
+        labels.add(label3);
+        labels.add(label4);
+        labels.add(label5);
+        labels.add(label6);
+        labels.add(label7);
+        labels.add(label8);
     }
 }
--- src/Main/Model/ThreadClient.java
+++ src/Main/Model/ThreadClient.java
@@ -1,29 +1,28 @@
 
 package Main.Model;
 
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ThreadClient extends Observable implements Runnable {
     private Socket socket;
     private DataInputStream bufferDeEntrada = null;
-    private TextArea log;
 
-    public ThreadClient(Socket socket, TextArea log) {
+    public ThreadClient(Socket socket) {
         this.socket = socket;
-        this.log = log;
     }
 
     public void run() {
 
         try {
             bufferDeEntrada = new DataInputStream(socket.getInputStream());
-
-            String st = "";
+            String mensaje = "";
             do {
                 try {
                     Thread.sleep(ThreadLocalRandom.current().nextLong(1000L)+100);
@@ -31,16 +30,15 @@ public class ThreadClient extends Observable implements Runnable {
                     e.printStackTrace();
                 }
                 try {
-                    st = bufferDeEntrada.readUTF();
-                    log.setText(st);
-                    String[] array = st.split(":");
-
+                    mensaje = bufferDeEntrada.readUTF();
+                    String[] datagrama;
+                    datagrama = mensaje.split(":");
                     this.setChanged();
-                    this.notifyObservers(st);
+                    this.notifyObservers(datagrama[1]);
                 } catch (IOException e) {
                     //e.printStackTrace();
                 }
-            }while (!st.equals("FIN"));
+            }while (!mensaje.equals("FIN"));
         } catch (IOException e) {
             e.printStackTrace();
         }
